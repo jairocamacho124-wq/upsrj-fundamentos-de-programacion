f67b4b11c2e3c5eadc998270f6489da13bd38799
jairocamacho124-wq <jairocamacho124@gmail.com>
2026-01-19 12:16:37 -0600

    entrega: AREA Y VOLUMEN

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 
+int main()
 
-int main(int argc, char *argv)
 {
-    printf("Hello world!\n");
+    float side, area, volume;
+    printf("Enter the value for the side:");
+    scanf("%f",&side);
+    area=side*side;
+    printf("The area of the square is:");
+    printf("%f",area);
+    volume=side*side*side;
+    printf("\n");
+    printf("The volume of cube is:");
+    printf("%f",volume);
+    printf("\n");
     return 0;
-}  
+} 
